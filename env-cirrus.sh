16c2ffc233007f646773cc4c89fd5a54e9c3b810
lparisi <l.parisi@epcc.ed.ac.uk>
2024-08-07 13:45:56 +0100

    Updated cirrus environment script

--- env-cirrus.sh
+++ env-cirrus.sh
@@ -1,10 +1,12 @@
+
+
+source /work/z04/z04/lparisi/courses/gpu-directives/clang/env.sh
 module load nvidia/nvhpc-nompi/24.5
-module load gcc
-module load intel-20.4/cmkl
+#module load gcc
+#module load intel-20.4/cmkl
 
 
-export CXX=nvc++
-
+#export CXX=nvc++
 
 run_gpu()
 {
