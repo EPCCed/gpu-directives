e5221c1dfa818e39ea5af2e42820bdce21602365
lucaparisi91 <luca.parisi91@gmail.com>
2025-03-04 15:39:30 +0000

    Corrected typo in Makefile

--- exercises/poisson/submit.sh
+++ exercises/poisson/submit.sh
@@ -3,15 +3,10 @@
 #SBATCH --job-name=poisson
 #SBATCH --gpus=1
 #SBATCH --nodes=1
-#SBATCH --exclusive
 #SBATCH --time=00:05:00
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu-shd
 
-WORK_DIR=$(pwd)
-cd ../../
-source env-archer2.sh 
-cd $WORK_DIR
 
 #export CRAY_ACC_DEBUG=3
 
