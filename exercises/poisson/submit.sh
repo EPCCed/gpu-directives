e37d1ba99e2070aa49f7c4e12a2608c5ea8dc4b9
eleanor-broadway <eleanor_broadway@outlook.com>
2025-03-07 16:16:10 +0000

    Solution to 3.3: Multiple Fields - attempt 1

--- exercises/poisson/submit.sh
+++ exercises/poisson/submit.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=poisson
+#SBATCH --job-name=poisson-gpu
 #SBATCH --gpus=1
 #SBATCH --nodes=1
-#SBATCH --exclusive
 #SBATCH --time=00:05:00
 #SBATCH --partition=gpu
-#SBATCH --qos=gpu-exc
+#SBATCH --qos=gpu-shd
 
 WORK_DIR=$(pwd)
 cd ../../
 source env-archer2.sh 
 cd $WORK_DIR
-
-#export CRAY_ACC_DEBUG=3
-
-
-srun --ntasks=1 --cpus-per-task=1  ./poisson
+srun --ntasks=1 --cpus-per-task=1  ./poisson_f90
